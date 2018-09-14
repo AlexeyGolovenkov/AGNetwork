e2a60677b858cfdf877329eb91fec7d92e798ce1
Alexey Golovenkov <mobidevelop@gmail.com>
2018-09-15 01:35:32 +0400

    Additional tests

--- AGNetwork/AGNetworkTests/AGNHTTPMethodTests.swift
+++ AGNetwork/AGNetworkTests/AGNHTTPMethodTests.swift
@@ -29,4 +29,31 @@ class AGNHTTPMethodTests: XCTestCase {
         let path = method.path(from: "https://example.com/", and: parameters)
         XCTAssertTrue(path == "https://example.com/", "Wrong path: \(path)")
     }
+    
+    func testGetBodyParameters() {
+        let method = AGNHTTPMethod.get
+        
+        let parameters: [String: Any] = ["name": "John",
+                                         "age": 21]
+        let body = method.body(for: parameters)
+        XCTAssertNil(body, "Body for GET must be nil")
+    }
+    
+    func testPostBodyParameters() {
+        let method = AGNHTTPMethod.post
+        
+        let parameters: [String: Any] = ["name": "John",
+                                         "age": 21]
+        guard let body = method.body(for: parameters) else {
+            XCTFail("Body can't be nil")
+            return
+        }
+        guard let parsedParameters = (try? JSONSerialization.jsonObject(with: body, options: [])) as? [String: Any] else {
+            XCTFail("body not parsed")
+            return
+        }
+        XCTAssertEqual(parameters.count, parsedParameters.count, "Parameters were parsed in a wrong way")
+        XCTAssertEqual(parameters["name"] as! String, parsedParameters["name"] as! String, "Parameters were parsed in a wrong way")
+        XCTAssertEqual(parameters["age"] as! Int, parsedParameters["age"] as! Int, "Parameters were parsed in a wrong way")
+    }
 }
